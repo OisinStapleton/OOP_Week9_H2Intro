655d642d58238405ebf6119a3170afc1a763e4d1
OisinStapleton <G00365445@atu.ie>
2026-04-02 13:55:14 +0100

    added getByDate

--- src/main/java/ie/atu/oop_lab9_h2intro/controller/ReservationController.java
+++ src/main/java/ie/atu/oop_lab9_h2intro/controller/ReservationController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -37,5 +38,11 @@ public class ReservationController {
     @GetMapping("{id}")
     public ResponseEntity<Reservation> getById(int id) {
         return ResponseEntity.ok(reservationService.getReservationById(id));
+    }
+    //Get one by date
+
+    @GetMapping ("/date/{date}")
+    public ResponseEntity<List<Reservation>> getByDate(@PathVariable LocalDate date) {
+        return ResponseEntity.ok(reservationService.getByDate(date));
     }
 }
--- src/main/java/ie/atu/oop_lab9_h2intro/respository/ReservationRepo.java
+++ src/main/java/ie/atu/oop_lab9_h2intro/respository/ReservationRepo.java
@@ -3,6 +3,10 @@ package ie.atu.oop_lab9_h2intro.respository;
 import ie.atu.oop_lab9_h2intro.model.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface ReservationRepo extends JpaRepository<Reservation,Integer> {
 
+    List<Reservation> findByReservationDate(LocalDate reservationDate);
 }
